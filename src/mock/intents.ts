da3925186a27daae24a2432d288f7d91b7031994
AmirDahmouni <dahmouni_amir@hotmail.fr>
2025-10-01 09:47:27 +0200

    fix/intent interface

--- src/mock/intents.ts
+++ src/mock/intents.ts
@@ -1,25 +1,31 @@
-export const intents = {
+type IntentProps = { [prop: string]: any }
+
+type Intents = {
+  [screenId: string]: IntentProps
+}
+
+export const intents: Intents = {
   "page-a": {
     "address-form": { default: "16 RUE DE LA VILLE LEVEQUE 75008 PARIS" },
     button: { label: "Envoyer" }
   },
-
   "page-b": {
-    "accept-cgu": {label: "J’accepte les CGU"},
+    "accept-cgu": { label: "J’accepte les CGU" },
     "address-form": {
-      default: "16 RUE DE LA VILLE LEVEQUE 75008 PARIS",
-      "visible-if": {"accept-cgu": true}
+      default: "5 Avenue Anatole France 75007 Paris",
+      "visible-if": { "accept-cgu": true }
     },
-    button: {label: "Envoyer"}
+    button: { label: "Valider" }
   }
-};
+}
+
 
 export function fetchIntents(screenId: string) {
   // Simulation d'un délais de réponse de la part du serveur entre 50ms et 1s
   return new Promise(resolve => {
     setTimeout(
       () => resolve(intents[screenId]),
-        50 + Math.random() * 950
-      )
+      50 + Math.random() * 950
+    )
   });
 }
